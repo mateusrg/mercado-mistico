d51182e5b6fa923e23512011d470673d8d6385fc
Mateus G. <mateusrgewehr@gmail.com>
2024-08-27 08:46:38 -0300

    remoção de algumas rotas post

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const chaveCriptografia = "ok-jfnxbmdukl-hzfnbd-sifkj"
+const chaveCriptografia = "ok-jfnxbmdukl-hzfnbd-sifkj";
 const porta = 3000;
 const app = express();
 app.use(cors());
@@ -53,29 +53,6 @@ app.get("/cupons", (request, response) => {
 
 app.get("/enderecos", (request, response) => {
     response.sendFile(path.join(__dirname, "..", "..", "frontend", "pages", "HTML", "endereco.html"));
-
-    const idUsuario = 1; // Não sei se está certo, ver na aula
-    const query = "SELECT * FROM Endereco WHERE idUsuario = ?";
-
-    connection.query(query, [idUsuario], (err, results) => {
-        if (err) {
-            response
-            .status(500)
-            .json({
-                success: false,
-                message: "Erro ao buscar endereços",
-                data: err
-            });
-        } else {
-            response
-            .status(200)
-            .json({
-                success: true,
-                message: "Endereços encontrados",
-                data: results
-            });
-        }
-    });
 });
 
 app.get("/favoritos", (request, response) => {
@@ -112,149 +89,30 @@ app.get("/usuario_adm", (request, response) => {
 
 // Métodos POST
 
-app.post('/enderecos/adicionar', autenticarToken, (request, response) => {
-    let params = [
-        request.body.cep,
-        request.body.endereco,
-        request.body.numero,
-        request.body.complemento,
-        request.body.bairro,
-        request.body.cidade,
-        request.body.estado,
-        1 // Aqui é o ID do usuário, não é esse 1
-    ];
 
-    let query = "INSERT INTO Endereco(CEP, endereco, numeroResidencia, complemento, bairro, cidade, estado, idUsuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
-    
-    connection.query(query, params, (err, results) => {
-        if (results) {
-            response
-            .status(201)
-            .json({
-                success: true,
-                message: "Endereço adicionado com sucesso",
-                data: results
-            });
-        } else {
-            response
-            .status(400)
-            .json({
-                success: false,
-                message: "Erro ao adicionar endereço",
-                data: err
-            });
-        }
-    });
-});
 
-app.post('/cadastro/adicionar', async (request, response) => {
-    const senhaCriptografada = await bcrypt.hash(request.body.senha, 10);
-    let params = [
-        request.body.nome,
-        request.body.email,
-        senhaCriptografada,
-        0
-    ];
+// function autenticarToken(request, response, next) {
+//     const token = request.body['token'];
 
-    let query = "INSERT INTO Usuario(nome, email, senha, administrador) VALUES (?, ?, ?, ?);";
-    
-    connection.query(query, params, (err, results) => {
-        if (results) {
-            response
-            .status(201)
-            .json({
-                success: true,
-                message: "Usuário cadastrado com sucesso",
-                data: results,
-                nome: request.body.nome
-            });
-        } else {
-            response
-            .status(400)
-            .json({
-                success: false,
-                message: "Erro ao cadastrar usuário",
-                data: err
-            });
-        }
-    });
-});
+//     if (token == null) {
+//         return response
+//         .status(401)
+//         .json({
+//             success: false,
+//             message: 'Token não encontrado'
+//         });
+//     }
 
-app.post('/login/verificar', (request, response) => {
-    const { email, senha } = request.body;
-
-    const query = 'SELECT * FROM Usuario WHERE email = ?';
-    const params = [email]
-    
-    connection.query(query, params, async (err, results) => {
-        if (err) {
-            console.error("Erro no servidor:", err);
-            return response
-                .status(500)
-                .json({
-                    success: false,
-                    message: 'Erro no servidor',
-                    data: err
-                });
-        }
-        
-        if (results.length === 0) {
-            return response
-                .status(401)
-                .json({
-                    success: false,
-                    message: 'Email não encontrado',
-                    data: err
-                });
-        }
-        
-        const user = results[0];
-        const senhaValida = await bcrypt.compare(senha, user.senha);
-        
-        if (senhaValida) {
-            const token = jwt.sign({id: user.idUsuario, nome: user.nome}, chaveCriptografia, {expiresIn: "1h"});
-            return response
-                .status(200)
-                .json({
-                    success: true,
-                    message: 'Login bem-sucedido',
-                    token: token, // tirei o data: results, pq não faz sentido criptografar a senha e devolvê-la pro client
-                    nome: user.nome
-                });
-        } else {
-            return response
-                .status(401)
-                .json({
-                    success: false,
-                    message: 'Senha incorreta'
-                });
-        }
-    });
-});
-
-function autenticarToken(request, response, next) {
-    const authHeader = request.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token == null) {
-        return response
-        .status(401)
-        .json({
-            success: false,
-            message: 'Token não encontrado'
-        });
-    }
-
-    jwt.verify(token, chaveCriptografia, (err, user) => {
-        if (err) {
-            return response
-            .status(403)
-            .json({
-                success: false,
-                message: 'Token inválido'
-            });
-        }
-        request.user = user;
-        next();
-    });
-}
+//     jwt.verify(token, chaveCriptografia, (err, user) => {
+//         if (err) {
+//             return response
+//             .status(403)
+//             .json({
+//                 success: false,
+//                 message: 'Token inválido'
+//             });
+//         }
+//         request.user = user;
+//         next();
+//     });
+// }
