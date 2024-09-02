abacccc75b38aa7adaf42e4ce71c9693a44fd0f1
VictorSantosReis12 <victorsantosreis6@gmail.com>
2024-09-02 19:32:40 -0300

    conexão com o banco no estoque

--- frontend/pages/JS/estoque.js
+++ frontend/pages/JS/estoque.js
@@ -41,3 +41,49 @@ function botaoFechar () {
     document.querySelectorAll('#precoInput').forEach(input => input.value = '');
     document.querySelectorAll('#editar_inputDescricao').forEach(input => input.value = '');
 }
+
+async function exibirProdutos() {
+    const produtos = await selecionarProdutos();
+    const lista_produtos = document.getElementById('secao_principal');
+    produtos.forEach(produto => {
+        let novoProduto = document.createElement('section');
+        novoProduto.innerHTML = `
+            <img src="${produto.imagem}" alt="Produto" id="imagemProduto">
+            <div class="div_info">
+                <h3 id="nome">${produto.nome}</h3>
+                <h3 id="quant">Quantidade: ${produto.quantidade}</h3>
+                <h3 id="preco">Preço: R$ ${produto.preco}</h3>
+            </div>
+            <div id="div_inputDescricao">
+                <label for="descricao" id="labelDescricao">Descrição:</label>
+                <textarea type="text" class="inputDescricao" id="inputDescricao" name="descricao" placeholder="Descrição aqui" textContent="${produto.descricao}" readonly></textarea>
+            </div>
+            <div class="div_editar" onclick="botaoEditar()">
+                <img src="../../assets/criar.png" alt="Editar" id="imagemEditar">
+                <h2 id="textoEditar">Editar</h2>
+            </div>
+            <div class="div_excluir">
+                <img src="../../assets/lixo.png" alt="Excluir" id="imagemExcluir">
+                <h2 id="textoExcluir">Excluir</h2>
+            </div>
+        `;
+        novoProduto.className = 'secao_produto';
+        lista_produtos.appendChild(novoProduto);
+    });
+}
+
+async function selecionarProdutos() {
+    const response = await fetch("/listar_produtos", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const results = await response.json();
+
+    if (results.success) {
+        return results.data;
+    }
+    alert(results.message);
+    return undefined;
+}
